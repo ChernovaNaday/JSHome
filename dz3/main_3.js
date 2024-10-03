07163c29171eefc6fcae29aa84f71c50c0c92a35
ChernovaNaday <chernovan2@mail.ru>
2024-10-03 13:16:28 +0300

    осталось56

---
+++ dz3/main_3.js
@@ -0,0 +1,27 @@
+// Создать массив с 5 размичными элементами
+
+// let animals= ["cat", "dog", "bird", "lion", "snake"];
+//  console.log(animals)   
+
+//  Добавить новое имя в конец
+// animals.push("newElement")
+
+//  Добавить новое имя в начало
+
+// animals.unshift("firstElement")
+
+// Удалить первое и последнее имя
+
+// animals.pop()
+// 'newElement'
+// let lastElement = animals.pop()
+
+// animals.shift()
+// 'first_element'
+
+// Сохранить длину получившегося массива в переменную lenOfList и вывести в консоль
+
+let animals= ["cat", "dog", "bird", "lion", "snake"];
+
+console.log(animals)
+animals.join(" ");
