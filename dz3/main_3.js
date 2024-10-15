aeb3219016b15606cba3f8fadbfc36a89a60ed80
ChernovaNaday <chernovan2@mail.ru>
2024-10-15 14:25:46 +0300

    готово

--- dz3/main_3.js
+++ dz3/main_3.js
@@ -1,29 +1,29 @@
-// // Создать массив с 5 размичными элементами
+// Создать массив с 5 размичными элементами
 
-// let animals= ["cat", "dog", "bird", "lion", "snake"];
-//  console.log(animals)   
+let animals= ["cat", "dog", "bird", "lion", "snake"];
+ console.log(animals)   
 
-// //  Добавить новое имя в конец
-// animals.push("newElement")
+//  Добавить новое имя в конец
+animals.push("newElement")
 
-// //  Добавить новое имя в начало
+//  Добавить новое имя в начало
 
-// animals.unshift("firstElement")
+animals.unshift("firstElement")
 
-// // Удалить первое и последнее имя
+// Удалить первое и последнее имя
 
-// animals.pop()
-// 'newElement'
-// let lastElement = animals.pop()
+animals.pop()
+'newElement'
+let lastElement = animals.pop()
 
-// animals.shift()
-// 'first_element'
+animals.shift()
+'first_element'
 
-// // Сохранить длину получившегося массива в переменную lenOfList и вывести в консоль
+// Сохранить длину получившегося массива в переменную lenOfList и вывести в консоль
 
-// let lenOfList ;
-// lenOfList = animals;
-// console.log(lenOfList)
+let lenOfList ;
+lenOfList = animals;
+console.log(lenOfList)
 
 
 
